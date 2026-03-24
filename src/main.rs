729b0b173866c16ce1f40c66f553135a41090911
Anon <ivancreeperman@gmail.com>
2026-03-24 17:46:42 +0300

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,21 @@ use std::io::{self, Write};
 
 fn main() {
     // TODO: Uncomment the code below to pass the first stage
-    print!("$ ");
+    loop {
+        print!("$ ");
 
-    // Rust doesn't flush afrer a print! only after println! so the print above won't be flushed
-    // unless we force it
-    io::stdout().flush().unwrap();
+        // Rust doesn't flush afrer a print! only after println! so the print above won't be flushed
+        // unless we force it
+        io::stdout().flush().unwrap();
 
-    let mut raw_cmd = String::new();
+        let mut raw_cmd = String::new();
 
-    io::stdin()
-        .read_line(&mut raw_cmd)
-        .expect("Failed to read user input");
+        io::stdin()
+            .read_line(&mut raw_cmd)
+            .expect("Failed to read user input");
 
-    // let cmd = raw_cmd.trim();
+        // let cmd = raw_cmd.trim();
 
-    println!("{}: command not found", raw_cmd.trim());
+        println!("{}: command not found", raw_cmd.trim());
+    }
 }
