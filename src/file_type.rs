1be57e196f87cb1daaf1041a1cece64c27d8deb9
Anon <29154527+Ivanko1608@users.noreply.github.com>
2026-03-29 19:00:03 +0300

    codecrafters submit [skip ci]

--- src/file_type.rs
+++ src/file_type.rs
@@ -1,12 +1,10 @@
-use std::{fs::DirEntry, io, path::PathBuf};
+use std::{io, path::PathBuf};
 
 pub fn is_executable(de: &PathBuf) -> Result<bool, io::Error> {
     #[cfg(unix)]
     {
         use std::os::unix::fs::PermissionsExt;
         let file_metadata = std::fs::metadata(de)?;
-        file_metadata.permissions().mode() & 0o100 != 0;
+        Ok(file_metadata.permissions().mode() & 0o100 != 0)
     }
-
-    Ok(false)
 }
